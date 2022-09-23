27cec54aa2f17526268044ca3fbc31a8b2976899
Lauren Metz <Laurenmetz15@gmail.com>
2022-09-22 21:57:16 -0400

    commit tree 5f19a7df84d414eb05d40e12fd38cdab642f6c4c

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,18 @@
 import React from "react";
+import Links from './Links.js';
 
-function About() {
+function About(props) {
+  let bioPresent = true; 
+  console.log(props.bio);
+  if (props.bio== undefined || props.bio == '') {
+    bioPresent = false;
+  }
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bioPresent ? <p>{props.bio}</p> : null};
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={props.github} linkedin={props.linkedin}></Links>
       {/* add your <Links /> component here */}
     </div>
   );
